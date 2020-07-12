00e193319e005bcd30509458b1866d38d1a04432
mikejeter <mjeter45@yahoo.com>
2020-07-12 17:08:23 -0400

    Writing Linear Search and Binary Search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -9,6 +11,20 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    start = 0
+    end = len(arr) - 1
 
+    while start < end:
+        middle = start + (end - start)
+        middle_item = arr[middle]
+        
+        if target == middle_item:
+            return middle
+            
+        elif target < middle_item:
+            end = middle - 1
+
+            
+                
 
     return -1  # not found
